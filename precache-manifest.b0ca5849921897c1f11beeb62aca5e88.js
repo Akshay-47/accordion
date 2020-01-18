6dcd145e400f86df0e62f840a6190750112e9d93
Akshay-47 <akshay.lokray47@gmail.com>
2020-01-18 12:58:33 +0530

    Updates

---
+++ precache-manifest.b0ca5849921897c1f11beeb62aca5e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185bb65aef3cd243f2e5dfefdf2c7f43",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "3c94423602f7a4b3bbf6",
+    "url": "/accordion/static/css/main.bdc28cb9.chunk.css"
+  },
+  {
+    "revision": "46cff6c428b7091f46b5",
+    "url": "/accordion/static/js/2.4946c375.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/accordion/static/js/2.4946c375.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c94423602f7a4b3bbf6",
+    "url": "/accordion/static/js/main.0e829892.chunk.js"
+  },
+  {
+    "revision": "d728db358ca7459f695a",
+    "url": "/accordion/static/js/runtime-main.2e0cbcd1.js"
+  }
+]);
